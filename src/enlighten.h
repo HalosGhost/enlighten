570451b8b284217768f9a740b80c290094ca1052
halosghost <halosghost@archlinux.info>
2016-04-06 16:55:37 -0500

    Add new usage output

--- src/enlighten.h
+++ src/enlighten.h
@@ -4,7 +4,6 @@
 #include <unistd.h>
 
 #define PROGNAME  "enlighten"
-#define USAGE_STR "Usage: " PROGNAME " <+|->\n"
 #define PERMS_STR PROGNAME ": That command must be run with root permissions\n"
 #define FAILED_TO PROGNAME ": Failed to "
 
@@ -12,6 +11,16 @@
 #define D_STEP 100
 #define D_DEV  "intel_backlight"
 #define D_PATH "/sys/class/backlight/" D_DEV "/brightness"
+
+static const char USAGE_STR [] =
+    "Usage: " PROGNAME " <command> (<argument>)\n"
+    PROGNAME " -- a very small backlight controller\n\n"
+    "Commands:\n"
+    "  help        print this help and exit\n"
+    "  get         print the current brightness and exit\n"
+    "  set LVL     set brightness to LVL and exit\n"
+    "  inc         increase brightness by step\n"
+    "  dec         decrease brightness by step\n";
 
 void
 bl_set (signed);

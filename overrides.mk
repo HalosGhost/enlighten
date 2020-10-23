2d6b410646329e237822da0b60b23206e8998d26
halosghost <samuel.stuewe@gmail.com>
2020-10-22 22:30:50 -0500

    Fix backlight.rules install; be more portable in backlight rules
    (using /usr/bin/env should be portable across more systems)

--- overrides.mk
+++ overrides.mk
@@ -2,7 +2,7 @@
 
 install: bin doc
 	@install -Dm755 $(BLDDIR)/$(PROGNM) $(BINDIR)/$(PROGNM)
-	@install -Dm644 '90-backlight.rules' $(LIBDIR)/udev/rules.d/'09-backlight.rules'
+	@install -Dm644 '90-backlight.rules' $(LIBDIR)/udev/rules.d/'90-backlight.rules'
 	@(for i in $(shell basename $(wildcard $(BLDDIR)/*.[0-9])); do \
 		section="$${i: -1}"; \
 		install -Dm755 $(BLDDIR)/"$$i" $(DOCDIR)/man"$$section"/"$$i"; \

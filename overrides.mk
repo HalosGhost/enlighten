c7db92f98be37e8dd8baa4b5dbe3034fae6e06aa
halosghost <samuel.stuewe@gmail.com>
2020-10-22 23:38:11 -0500

    the dependency on `bin`/`doc` causes `install` to errantly rebuild

--- overrides.mk
+++ overrides.mk
@@ -1,6 +1,6 @@
 .PHONY: install uninstall
 
-install: bin doc
+install:
 	@install -Dm755 $(BLDDIR)/$(PROGNM) $(BINDIR)/$(PROGNM)
 	@install -Dm644 '90-backlight.rules' $(LIBDIR)/udev/rules.d/'90-backlight.rules'
 	@(for i in $(shell basename $(wildcard $(BLDDIR)/*.[0-9])); do \
@@ -8,7 +8,7 @@ install: bin doc
 		install -Dm755 $(BLDDIR)/"$$i" $(DOCDIR)/man"$$section"/"$$i"; \
 	done)
 
-uninstall: doc
+uninstall:
 	@rm -f -- $(BINDIR)/$(PROGNM)
 	@rm -f -- $(LIBDIR)/udev/rules.d/'90-backlight.rules'
 	@(for i in $(shell basename $(wildcard $(BLDDIR)/*.[0-9])); do \
